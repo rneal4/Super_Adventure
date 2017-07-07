943b0818f539f9ffc0bc9a478566f93c706719ec
Richard Neal <rwneal.richard@gmail.com>
2017-07-07 15:25:50 -0400

    Add ToXMLString for Player.cs

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Engine
 {
@@ -87,6 +88,71 @@ namespace Engine
 
             if (playerQuest != null)
                 playerQuest.IsCompleted = true;
+        }
+
+        public string ToXMLString()
+        {
+            XmlDocument playerData = new XmlDocument();
+
+            XmlNode player = playerData.CreateElement("Player");
+            playerData.AppendChild(player);
+
+            XmlNode stats = playerData.CreateElement("Stats");
+            player.AppendChild(stats);
+
+            XmlNode currentHitPoints = playerData.CreateElement("CurrentHitPoints");
+            currentHitPoints.AppendChild(playerData.CreateTextNode(CurrentHitPoints.ToString()));
+            stats.AppendChild(currentHitPoints);
+
+            XmlNode maximumHitPoints = playerData.CreateElement("MaximumHitPoints");
+            maximumHitPoints.AppendChild(playerData.CreateTextNode(MaximumHitPoints.ToString()));
+            stats.AppendChild(maximumHitPoints);
+
+            XmlNode gold = playerData.CreateElement("Gold");
+            gold.AppendChild(playerData.CreateTextNode(ExperiencePoints.ToString()));
+            stats.AppendChild(gold);
+
+            XmlNode currentLocation = playerData.CreateElement("CurrentLocation");
+            currentLocation.AppendChild(playerData.CreateTextNode(CurrentLocation.ID.ToString()));
+            stats.AppendChild(currentLocation);
+
+            XmlNode inventoryItems = playerData.CreateElement("InventoryItems");
+            player.AppendChild(inventoryItems);
+
+            foreach (InventoryItem item in Inventory)
+            {
+                XmlNode inventoryItem = playerData.CreateElement("InventoryItem");
+
+                XmlAttribute idAttribute = playerData.CreateAttribute("ID");
+                idAttribute.Value = item.Details.ID.ToString();
+                inventoryItem.Attributes.Append(idAttribute);
+
+                XmlAttribute quantityAttribute = playerData.CreateAttribute("Quantity");
+                quantityAttribute.Value = item.Quantity.ToString();
+                inventoryItem.Attributes.Append(quantityAttribute);
+
+                inventoryItems.AppendChild(inventoryItem);
+            }
+
+            XmlNode playerQuests = playerData.CreateElement("PlayerQuest");
+            player.AppendChild(playerQuests);
+
+            foreach (PlayerQuest quest in Quests)
+            {
+                XmlNode playerQuest = playerData.CreateElement("PlyaerQuest");
+
+                XmlAttribute idAttribute = playerData.CreateAttribute("PlayerQuest");
+                idAttribute.Value = quest.Details.ID.ToString();
+                playerQuest.Attributes.Append(idAttribute);
+                
+                XmlAttribute isCompletedAttribute = playerData.CreateAttribute("IsCompleted");
+                isCompletedAttribute.Value = quest.IsCompleted.ToString();
+                playerQuest.Attributes.Append(isCompletedAttribute);
+
+                playerQuests.AppendChild(playerQuest);
+            }
+
+            return playerData.InnerXml;
         }
     }
 }
