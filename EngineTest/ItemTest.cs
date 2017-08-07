c1a18178f25325de1e4254ce419027e59ab86efb
Richard Neal <rwneal.richard@gmail.com>
2017-08-07 13:57:48 -0400

    Added More Unit Test Classes

---
+++ EngineTest/ItemTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EngineTest
+{
+    [TestClass]
+    public class ItemTest
+    {
+        [TestMethod]
+        public void IsSellable_False()
+        {
+            Item item = World.ItemByID(World.ITEM_ID_ADVENTURER_PASS);
+
+            Assert.IsFalse(item.IsSellable);
+        }
+    }
+}
