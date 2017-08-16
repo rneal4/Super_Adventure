847c5e3178efc3a06be8e4817ab58f3a45d0656f
Richard Neal <rwneal.richard@gmail.com>
2017-08-16 15:44:08 -0400

    Added Test for PlayerQuest

---
+++ EngineTest/PlayerQuestTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Engine;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.IO;
+
+namespace EngineTest
+{
+    [TestClass]
+    public class PlayerQuestTest
+    {
+        [TestMethod]
+        public void Quest_EventRaised_DetailsChanged()
+        {
+            PlayerQuest _playerQuest = new PlayerQuest(null);
+
+
+
+        }
+    }
+}
